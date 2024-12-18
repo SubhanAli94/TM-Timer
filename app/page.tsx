378cb35dd3b04ca7eb0037f0d8d5a564004dd5b2
SubhanAli94 <subhanali.mbzuai@gmail.com>
2024-12-18 17:49:32 +0400

    Adjusted top margin for title

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Timer from '@/components/timer'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center pt-10">
+    <main className="flex min-h-screen flex-col items-center pt-8">
       <div className="flex justify-start items-start w-full">
         <h1 className="text-3xl ml-8">
           <span className="font-bold">TM</span> Timer
